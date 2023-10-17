98b3cc3feba472028e744ee69f26e749eb58129b
MonkeyCodeMen <stefan.menninger@gmx.de>
2023-10-17 13:13:09 +0200

    clamp and dim color test

--- test/helperTest.cpp
+++ test/helperTest.cpp
@@ -2,17 +2,107 @@
 #include "unity.h"
 
 /*
-u32_t convertStrToInt(const char * str);
-u32_t convertStrToInt(String str);
-
-u32_t clamp(u32_t lowEnd,u32_t value,u32_t highEnd);
 
 u16_t toColor565(u8_t r,u8_t g,u8_t b);
 u16_t toColor565(u32_t c);
 
+u32_t dimColor255(u32_t color,u8_t dim );
+u32_t dimColor255(u8_t r,u8_t g,u8_t b,u8_t dim );
+inline u8_t  dimColorChannel255(u8_t c,u8_t dim) {    return ((u32_t)c*dim)/255; };
+u32_t dimRgb24ToRgb(u32_t value);
+
 u32_t get888ColorWheel(u8_t pos);
 
 */
+
+
+void test_helper_dimColor(void){
+    TEST_ASSERT_TRUE(dimColorChannel255(0,0)     ==   0 );
+
+    TEST_ASSERT_TRUE(dimColorChannel255(255,0)   ==   0 );
+    TEST_ASSERT_TRUE(dimColorChannel255(255,50)  ==  50 );
+    TEST_ASSERT_TRUE(dimColorChannel255(255,100) == 100 );
+    TEST_ASSERT_TRUE(dimColorChannel255(255,255) == 255 );
+
+    TEST_ASSERT_TRUE(dimColorChannel255(  0,255) ==   0 );
+    TEST_ASSERT_TRUE(dimColorChannel255( 42,255) ==  42 );
+    TEST_ASSERT_TRUE(dimColorChannel255(100,255) == 100 );
+
+    TEST_ASSERT_TRUE(dimColorChannel255(100,128) ==  50 );
+    TEST_ASSERT_TRUE(dimColorChannel255(200,128) == 100 );
+    
+
+
+}
+
+
+void test_helper_clamp(void){
+    TEST_ASSERT_TRUE(clamp(0,42,255)    == 42);
+    TEST_ASSERT_TRUE(clamp(100,42,255)  == 100);
+    TEST_ASSERT_TRUE(clamp(100,300,255) == 255);
+    TEST_ASSERT_TRUE(clamp(100,100,255) == 100);
+    TEST_ASSERT_TRUE(clamp(100,255,255) == 255);
+}
+
+void test_helper_convertStrToIntDec(void){
+    TEST_ASSERT_TRUE(convertStrToInt("12345678") == 12345678);
+    TEST_ASSERT_TRUE(convertStrToInt("  12345678") == 12345678);
+    TEST_ASSERT_TRUE(convertStrToInt(" 12345678") == 12345678);
+    TEST_ASSERT_TRUE(convertStrToInt(" 12345678  ") == 12345678);
+    TEST_ASSERT_TRUE(convertStrToInt(" 1 2345 678  ") == 12345678);
+
+
+    String str;
+    str = "42";    
+    TEST_ASSERT_TRUE(convertStrToInt(str) == 42);
+
+    TEST_ASSERT_TRUE(convertStrToInt("  ") == 0);
+    TEST_ASSERT_TRUE(convertStrToInt("0") == 0);
+    TEST_ASSERT_TRUE(convertStrToInt("") == 0);
+    TEST_ASSERT_TRUE(convertStrToInt(" 1 2345 678 € ") == 0);
+    TEST_ASSERT_TRUE(convertStrToInt(" 1.2345.678 ") == 0);
+
+    TEST_ASSERT_TRUE(convertStrToInt(" 1  ")             == 1);
+    TEST_ASSERT_TRUE(convertStrToInt(" 20  ")            == 20);
+    TEST_ASSERT_TRUE(convertStrToInt(" 300  ")           == 300);
+    TEST_ASSERT_TRUE(convertStrToInt(" 4000  ")          == 4000);
+    TEST_ASSERT_TRUE(convertStrToInt(" 50 000  ")        == 50000);
+    TEST_ASSERT_TRUE(convertStrToInt(" 600 000  ")       == 600000);
+    TEST_ASSERT_TRUE(convertStrToInt(" 7 000 000  ")     == 7000000);
+    TEST_ASSERT_TRUE(convertStrToInt(" 80 000 000  ")    == 80000000);
+    TEST_ASSERT_TRUE(convertStrToInt(" 900 000 000  ")   == 900000000);
+
+    TEST_ASSERT_TRUE(convertStrToInt(" 4 294 967 295  ") == 0xFFFFFFFF);
+    TEST_ASSERT_TRUE(convertStrToInt(" 4 294 967 296  ") == 0);
+    TEST_ASSERT_TRUE(convertStrToInt("14 000 000 000  ") == 0);
+}
+
+void test_helper_convertStrToIntHex(void){
+    TEST_ASSERT_TRUE(convertStrToInt("0x12345678")      == 0x12345678);
+    TEST_ASSERT_TRUE(convertStrToInt("  0x1234")        == 0x1234);
+    TEST_ASSERT_TRUE(convertStrToInt(" 0xdeAdBeEF")     == 0xdeadbeef);
+    TEST_ASSERT_TRUE(convertStrToInt(" 0X Dead Beef  ") == 0xdeadbeef);
+
+    String str;
+    str = "0x42";    
+    TEST_ASSERT_TRUE(convertStrToInt(str) == 0x42);
+
+    TEST_ASSERT_TRUE(convertStrToInt("0x") == 0);
+    TEST_ASSERT_TRUE(convertStrToInt("0xAAAAX") == 0);
+
+    TEST_ASSERT_TRUE(convertStrToInt(" 0x0  ")           == 0);
+    TEST_ASSERT_TRUE(convertStrToInt(" 0xFFFF FFFF  ")   == 0xFFFFFFFF);
+}
+
+
+void test_helper_convertStrToIntNeg(void){
+    TEST_ASSERT_TRUE((s32_t) convertStrToInt("-1")              == -1);
+    TEST_ASSERT_TRUE((s32_t) convertStrToInt("  - 233 ")        == -123);
+    TEST_ASSERT_TRUE((s32_t) convertStrToInt("-  1 233")        == -1233);
+
+    TEST_ASSERT_TRUE((s32_t) convertStrToInt("-0")              == 0);
+    TEST_ASSERT_TRUE((s32_t) convertStrToInt("-1233-")          == 0);
+}
 
 
 void test_helper_u32_byteAcess(void) {
@@ -123,5 +213,11 @@ void test_collection_helper(void) {
   RUN_TEST(test_helper_H_WORD);
   RUN_TEST(test_helper_u32_byteAcess);
   RUN_TEST(test_helper_u16_byteAcess);
+
+  RUN_TEST(test_helper_convertStrToIntDec);
+  RUN_TEST(test_helper_convertStrToIntHex);
+  RUN_TEST(test_helper_convertStrToIntNeg);
+
+  RUN_TEST(test_helper_clamp);
   
 }
