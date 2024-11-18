67df8fd38b3d99fb5aa44e3c651976970a62c161
MonkeyCodeMen <stefan.menninger@gmx.de>
2024-11-18 21:15:23 +0100

    new random file selection based on subDir structure

--- lib/LedCtrl/NeoMatrixCalendar.hpp
+++ lib/LedCtrl/NeoMatrixCalendar.hpp
@@ -2,7 +2,8 @@
 
 #include <NeoMatrixGif.hpp>
 #include <BufferedClock.hpp>
-#include <vector.hpp>
+#include <vector>
+#include <helper.h>
 
 /*
     class to implement an advents calendar based on NeoMatrixGif
@@ -20,11 +21,66 @@
     SDFile is reetrant safe 
     SD use a separate bus (special ussage of SPI ) ==> no MUTEX needed  for SD access
 
+
+    dir structure:
+
+
+    +--/calendar 
+       +--add/
+       |  +-- pic1.gif
+       |  +-- pic2.gif
+       +--def/
+       |  +--add/
+       |  |  +-- pic3.gif
+       |  |  +-- pic4.gif
+       |  +-- pic6.gif
+       |  +-- pic7.gif
+       |  +-- pic8.gif
+       +--01/
+       |  +--add/
+       |  |  +-- 01add1.gif
+       |  |  +-- 01add2.gif
+       |  +-- day_01a.gif
+       |  +-- day_01b.gif
+       +--02/
+       |  +--add/
+       |  |  +-- 02add1.gif
+       |  |  +-- 02add2.gif
+       |  +-- day_02a.gif
+       |  +-- day_02b.gif
+
+
+    base: calendar/        
+        base directory of calendar
+    
+    daily: 01/ 02/  def/
+        one sub dir for every day
+        in the daily directory will be shown with same probability (1- all other probability)
+
+        additional for this day:  01/add/   02/add/   def/add/
+            animations from this subdirectories will be shown with probability : DAILY_ADD_PROBABILITY  (0-100)
+
+    global additional:  add/
+        animations from this subdirectories will be shown with probability : GLOBAL_ADD_PROBABILITY  (0-100)
+
+        in each directory there can be an additional subdirectory, 
+            the files from this directory will be selected with a certain probability
+        
+
 */
+
+#define NEO_MATRIX_CALENDAR_WEIGHT_DAILY        100
+#define NEO_MATRIX_CALENDAR_WEIGHT_DAILY_ADD    10
+#define NEO_MATRIX_CALENDAR_WEIGHT_GLOBAL_ADD   1
+
 
 class MatrixGifCalendarAni : public MatrixGifFileAni{
     public:
-        MatrixGifCalendarAni()  {
+        MatrixGifCalendarAni() :
+            _weightDaily(100),
+            _weightDailyAdd(10),
+            _weightGlobalAdd(1),
+            _weightTotal(_weightDaily+_weightDailyAdd+_weightGlobalAdd) {
             NeoMatrixAni("calendar");
             _clearAll();    // function instead of init list, because will be used several time
         };    
@@ -34,50 +90,163 @@ class MatrixGifCalendarAni : public MatrixGifFileAni{
         }
 
         int  config(AniCfg cfg){
+            _state = stop;
+
+            // setup dir structure names
             StringList *    pList;
             pList = new StringList(cfg.str.c_str(),"#~#");
             _dirBase = pList->getNextListEntry();
-            _dirBase = removeLeadingCharacters(' ');
-            _dirBase = removeTrailingCharacters(' ');
-
-            _dirAdditional = _dirBase + "add/";
+            _dirBase = removeLeadingCharacters(_dirBase,' ');
+            _dirBase = removeTrailingCharacters(_dirBase,' ');
+            _dirGlobalAdd  = _dirBase + "add/";
             _dirDefault    = _dirBase + "def/";
+            _dirDefaultAdd = _dirBase + "def/add/";
 
             // test required path
             if ( (_testDirectory(_dirBase) == false)       ||
-                 (_testDirectory(_dirAdditional) == false) ||
+                 (_testDirectory(_dirGlobalAdd) == false)  ||
                  (_testDirectory(_dirDefault) == false)       ){
                 _clearAll();
                 return ANI_ERROR_FILE_NOT_FOUND;
             }
+            _addFilesToList(_dirDefault    ,".gif", _defaultFileList   ,false ,100);
+            _addFilesToList(_dirDefaultAdd ,".gif", _defaultAddFileList,false ,100);
+            _addFilesToList(_dirGlobalAdd  ,".gif", _globalAddFileList ,true  ,100);
 
             // test if default directory is not empty
-            _selectDirectory(_dirDefault);
-            if (_currentFileList.empty()){
+            if (_defaultFileList.empty()){
+                _clearAll();
                 return ANI_ERROR_FILE_NOT_FOUND;
             }
 
             // now select the daily directory (fallback to default if not exist)
-            _selectDirectory();
-            
+            _selectDirectory(true);
+
+            _state = init;
             return ANI_OK;
         }
 
-    protected:      
-        String                      _dirBase,_dirAdditional,_dirDefault;
-        String                      _currentDir,_currentFile;
-        DateTime                    _currentDate;
-        const std::vector<String>   _currentFileList;
+        void loop(uint32_t time,Adafruit_NeoMatrix * pMatrix){
+            u16_t color;
+            u8_t dim;
+            int nr;
 
+            switch (_state){
+                case stop:
+                    // do nothing parameters are loocked by other thread
+                    // the setup will move forward to state init
+                    break;
+
+                case init:
+                    Ani::loop(time);
+                    pMatrix->fillScreen(toColor565(getColor()));
+                    pMatrix->setBrightness(getDim());
+
+                    _fileIndex = 0;
+                    _loopIndex = 0;
+
+                    _selectNextFile();
+                    _gif.close();
+                    _gif.open((const char *)_currentFile.c_str(),_GIFOpenFile, _GIFCloseFile, _GIFReadFile, _GIFSeekFile, _GIFDraw);
+
+                    _lastFrame = time;
+                    _wait = 1;
+
+                    _state = run;
+                    break;
+                
+                case run:
+                    Ani::loop(time);
+                    if (time-_lastFrame >= _wait){
+                        _lastFrame = time;
+                        if (hasChanged()){
+                            pMatrix->setBrightness(getDim());
+                        }
+                        int res = _gif.playFrame(false,&_wait,pMatrix);
+                        if(res == 0){
+                            // last gif frame has been played 
+                            _gif.close();
+                            _selectNextFile();
+                            _gif.open((const char *)_currentFile.c_str(),_GIFOpenFile, _GIFCloseFile, _GIFReadFile, _GIFSeekFile, _GIFDraw);
+                            pMatrix->fillScreen(toColor565(getColor()));
+                        } else if (res == -1){
+                            _frameError();
+                        }
+                    }
+                    break;
+            }
+        }
+
+
+    protected:      
+        String                      _dirBase,_dirGlobalAdd,_dirDefault,_dirDefaultAdd;
+        String                      _dirDaily,_dirDailyAdd,_currentFile;
+        DateTime                    _currentDate;
+        DateTime                    _lastDate;
+        std::vector<String>         _dailyFileList;
+        std::vector<String>         _dailyAddFileList;
+        std::vector<String>         _defaultFileList;
+        std::vector<String>         _defaultAddFileList;
+        std::vector<String>         _globalAddFileList;
+
+        uint32_t                    _weightTotal;
+        uint32_t                    _weightDaily;
+        uint32_t                    _weightDailyAdd;
+        uint32_t                    _weightGlobalAdd;
+        
 
         void _clearAll(){
             _dirBase        = "";
-            _dirAdditional  = "";
+            _dirGlobalAdd   = "";
             _dirDefault     = "";
-            _currentDir     = "";
+            _dirDefaultAdd  = "";
+            _dirDaily       = "";
+            _dirDailyAdd    = "";
             _currentFile    = "";
-            _currentDate    = bufferedClock.getLoopDateTime();
+            _lastDate       = DateTime(2000,1,1,0,0,0);
+            _currentDate    = DateTime(2000,1,2,0,0,0);
+            _dailyFileList.clear();
+            _dailyAddFileList.clear();
+            _defaultFileList.clear();
+            _defaultAddFileList.clear();
+            _globalAddFileList.clear();
+            
             _state          = stop;
+        }
+
+        void _selectNextFile(){
+            // update dirs if new date (will be checked in function)
+            _selectDirectory();
+
+            // Generate a random number between 0 and totalWeight - 1
+            uint16_t randomValue = random(_weightTotal);
+
+            // Select the appropriate list based on the random value
+            if (randomValue < _weightDaily) {
+                // standard daily animation
+                int index = random(_dailyFileList.size());
+                _currentFile = _dailyFileList[index];
+            } else if (randomValue < (_weightDaily + _weightDailyAdd)) {
+                // daily additional animation
+                int count = _dailyAddFileList.size();
+                if (count > 0){
+                    int index = random(count);
+                    _currentFile = _dailyAddFileList[index];
+                } else {
+                    int index = random(_dailyFileList.size());
+                    _currentFile = _dailyFileList[index];
+                }
+            } else {
+                // global additional animation
+                int count = _globalAddFileList.size();
+                if (count > 0){
+                    int index = random(count);
+                    _currentFile = _globalAddFileList[index];
+                } else {
+                    int index = random(_dailyFileList.size());
+                    _currentFile = _dailyFileList[index];
+                }
+            }
         }
 
         bool _testDirectory(String path){
@@ -90,45 +259,70 @@ class MatrixGifCalendarAni : public MatrixGifFileAni{
             return false;
         }
 
-        void _selectDirectory(){
+        void _selectDirectory(bool forceNew = false){
+            _currentDate = bufferedClock.getLoopDateTime();
             uint8_t day = _currentDate.day();
-            if (day < 10){
-                _currentDir = _dirBase + "0" + String(day,DEC) + "/";
-            } else {
-                _currentDir = _dirBase + String(day,DEC) + "/";
+
+            if ((day == _lastDate.day()) && (forceNew == false)){
+                // no change in day number
+                return;
             }
 
+            if (day < 10){
+                _dirDaily = _dirBase + "0" + String(day,DEC) + "/";
+            } else {
+                _dirDaily = _dirBase + String(day,DEC) + "/";
+            }
+
+            _dirDailyAdd= _dirDaily + "add/";
+            _dailyFileList.clear();
+            _dailyAddFileList.clear();
+            _addFilesToList(_dirDaily     ,".gif"  ,_dailyFileList    ,false, 100);
+            _addFilesToList(_dirDailyAdd  ,".gif"  ,_dailyAddFileList ,false, 100);
+
+            if (_dailyFileList.empty()){
+                // fallback to default directory
+                String msg = "current dir is empty {" + _dirDaily + "} .. fallback to default";
+                LOG(msg.c_str());
+                _dirDaily         = _dirDefault;
+                _dailyFileList    = _defaultFileList; 
+                _dirDailyAdd      = _dirDefaultAdd;
+                _dailyAddFileList = _defaultAddFileList;
+            } 
         }
 
-        void _selectDirector(String& path){
+        void _addFilesToList(const String& path, const String& ext,std::vector<String>& fileList,bool recursive = false,int minSize=0){
+            SDFile dir = globalSDcard0.open(path.c_str());
+            if (!dir || !dir.isDirectory()) {
+                String msg = "Error: Unable to open directory:" + path ;
+                LOG(msg.c_str());
+                return;
+            }
 
+            // dir walk
             while (true) {
-                SDFile entry =  dir.openNextFile();
-                if (! entry) {
-                    // no more files
-                    break;
+                File entry = dir.openNextFile();
+                if (!entry) {
+                    break; // no more entries
                 }
 
-                for (uint8_t i = 0; i < numTabs; i++) {
-                    Serial.print('\t');
-                }
-
-                Serial.print(entry.name());
                 if (entry.isDirectory()) {
-                    Serial.println("/");
-                    printDirectory(entry, numTabs + 1);
+                    // do nothing == no recursive walk !!!
+                    if (recursive==true){
+                        _addFilesToList(path + "/" + entry.name(), ext, fileList);
+                    }
                 } else {
-                    // files have sizes, directories do not
-                    Serial.print("\t\t");
-                    Serial.println(entry.size(), DEC);
+                    // check for GIF file and filesize > 0
+                    String fileName = entry.name();
+                    if (fileName.endsWith(ext) && entry.size() >= minSize) {
+                        // add file name to list 
+                        fileList.push_back(path + "/" + fileName);
+                    }
                 }
                 entry.close();
             }
-            printDirectory(root,0);
-            return false;
+            dir.close();
         }
-        
-
-
 
 };
+
